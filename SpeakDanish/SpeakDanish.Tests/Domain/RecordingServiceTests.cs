ab1d369721f0262b9a8f73e6e54c4d5d6e1d988f
Vegard Aaberge <vegard.aaberge@gmail.com>
2023-01-06 18:25:50 +0800

    Add some new tests and setup the Fixture in the constructor

--- SpeakDanish/SpeakDanish.Tests/Domain/RecordingServiceTests.cs
+++ SpeakDanish/SpeakDanish.Tests/Domain/RecordingServiceTests.cs
@@ -10,18 +10,37 @@ using System.Collections.Generic;
 using SpeakDanish.Tests.Data;
 using System.Linq;
 using SpeakDanish.Data.Models;
+using MathNet.Numerics.Distributions;
 
 namespace SpeakDanish.Tests
 {
     public class RecordingServiceTests : IClassFixture<SpeakDanishDatabaseFixture>
     {
-        private SpeakDanishDatabaseFixture _fixture;
-        private IRecordingService _recordingService;
+        private readonly IRecordingService _recordingServiceManySentences;
+        private readonly IRecordingService _recordingServiceTwoSentences;
 
-        public RecordingServiceTests(SpeakDanishDatabaseFixture fixture)
+        private readonly string[] twoSentences = new string[] { "This is the previous sentence.", "This is another sentence." };
+        private readonly string[] manySentences = Enumerable.Range(1, 200).Select(x => $"Sentence {x}").ToArray();
+
+        public RecordingServiceTests(
+            SpeakDanishDatabaseFixture fixtureManySentences,
+            SpeakDanishDatabaseFixture fixtureTwoSentences)
         {
-            _fixture = fixture;
-            _recordingService = new RecordingService(fixture.DatabaseMock.Object);
+            SetupFixture(ref _recordingServiceManySentences, fixtureManySentences, manySentences);
+            SetupFixture(ref _recordingServiceTwoSentences, fixtureTwoSentences, twoSentences);
+        }
+
+        private void SetupFixture(ref IRecordingService recordingService, SpeakDanishDatabaseFixture fixture, string[] sentences)
+        {
+            recordingService = new RecordingService(fixture.DatabaseMock.Object);
+
+            List<SentenceEntity> sentenceEntities = sentences
+                .Select(sentence => new SentenceEntity
+                {
+                    Sentence = sentence
+                }).ToList();
+
+            fixture.AddAllItem(sentenceEntities);
         }
 
         [Fact]
@@ -93,37 +112,69 @@ namespace SpeakDanish.Tests
         public async Task TestGetRandomSentence_ValidPreviousSentence()
         {
             // Arrange
-            string[] sentences = new string[] { "This is the previous sentence.", "This is another sentence." };
-            List<SentenceEntity> sentenceEntities = sentences.Select(sentence => new SentenceEntity
-            {
-                Sentence = sentence
-            }).ToList();
-            _fixture.AddAllItem(sentenceEntities);
-            string previousSentence = sentences[0];
-            string exptected = sentences[1];
-            Task<string> getSentencesFromResources = Task.FromResult(string.Join("\n", sentences));
+            string previousSentence = twoSentences[0];
+            string exptected = twoSentences[1];
+            Task<string> getSentencesFromResources = Task.FromResult(string.Join("\n", twoSentences));
 
             // Act
-            string result = await _recordingService.GetRandomSentence(previousSentence, getSentencesFromResources);
+            string result = await _recordingServiceTwoSentences.GetRandomSentence(previousSentence, getSentencesFromResources);
 
             // Assert
             result.Should().NotBeNullOrWhiteSpace();
             result.Should().NotBe(previousSentence);
-            result.Should().ContainAny(sentences);
+            result.Should().ContainAny(twoSentences);
             result.Should().Be(exptected);
         }
 
         [Fact]
-        public async Task TestGetRandomSentence_InvalidPreviousSentence()
+        public async Task TestGetRandomSentence_ShouldFetchResource()
         {
             // Arrange
-            // TODO
+            string previousSentence = twoSentences[0];
+            string[] resourceSentences = new string[] { "This is a sentence." };
+            Task<string> getSentencesFromResources = Task.FromResult(string.Join("\n", resourceSentences));
 
             // Act
-            // TODO
+            string result = await _recordingServiceTwoSentences.GetRandomSentence("", getSentencesFromResources);
 
             // Assert
-            // TODO
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be(resourceSentences.First());
+        }
+
+        [Fact]
+        public async Task TestGetRandomSentence_ShouldBeRandom()
+        {
+            // Arrange
+            string[] sentences = Enumerable.Range(1, 200).Select(x => $"Sentence {x}").ToArray();
+            
+            Task<string> getSentencesFromResources = Task.FromResult(string.Join("\n", sentences));
+
+            int numTrials = 20;
+            Dictionary<string, int> observedFrequencies = sentences.ToDictionary(key => key, value => 0);
+
+            // Act
+            for (int i = 0; i < numTrials; i++)
+            {
+                var selectedSentence = await _recordingServiceManySentences.GetRandomSentence("", getSentencesFromResources);
+                observedFrequencies[selectedSentence] = observedFrequencies[selectedSentence] + 1;
+            }
+
+            // Assert
+            double chiSquaredValue = 0.0;
+            double expectedFrequency = 1.0 / sentences.Count();
+            foreach (var sentence in sentences)
+            {
+                double observedFrequency = (double)observedFrequencies[sentence] / sentences.Count();
+                double diff = observedFrequency - expectedFrequency;
+                chiSquaredValue += diff * diff / expectedFrequency;
+            }
+
+            int degreesOfFreedom = sentences.Count() - 1;
+            ChiSquared chiSquared = new ChiSquared(degreesOfFreedom);
+            double pValue = 1 - chiSquared.CumulativeDistribution(chiSquaredValue);
+
+            pValue.Should().BeGreaterThan(0.05);
         }
 
         [Fact]
