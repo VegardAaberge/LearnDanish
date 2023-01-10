72401c9ee2ca4483ae60a397e61423371ad85ce5
Vegard Aaberge <vegard.aaberge@gmail.com>
2023-01-10 15:40:50 +0800

    Remove dependency injection in the unit tests

---
+++ SpeakDanish/SpeakDanish.Tests/Domain/UseCases/AudioUseCasesBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using SpeakDanish.Contracts.Platform;
+using SpeakDanish.Domain.UseCases;
+using Xamarin.Essentials;
+using Xamarin.Essentials.Interfaces;
+
+namespace SpeakDanish.Tests.Domain.UseCases
+{
+	public class AudioUseCaseBuilder
+	{
+        public Mock<IPermissions> Permissions = new Mock<IPermissions>();
+        public Mock<ITtsDataInstaller> TtsDataInstaller = new Mock<ITtsDataInstaller>();
+        public Mock<IAudioRecorder> AudioRecorder = new Mock<IAudioRecorder>();
+        public Mock<ITextToSpeech> TextToSpeech = new Mock<ITextToSpeech>();
+
+        public AudioUseCase AudioUseCase { get; set; }
+
+        public AudioUseCaseBuilder Build()
+        {
+            AudioUseCase = new AudioUseCase(
+                Permissions.Object,
+                TtsDataInstaller.Object,
+                AudioRecorder.Object,
+                TextToSpeech.Object
+            );
+            return this;
+        }
+
+        public AudioUseCaseBuilder WithSpeakAsync(List<Locale> locales)
+        {
+            TextToSpeech
+                    .Setup(tts => tts.GetLocalesAsync())
+                    .ReturnsAsync(locales);
+            TextToSpeech
+                .Setup(tts => tts.SpeakAsync(It.IsAny<string>(), It.IsAny<SpeechOptions>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+            return this;
+        }
+
+        public AudioUseCaseBuilder WithStoragePermission(PermissionStatus status, PermissionStatus response)
+        {
+            Permissions
+                .Setup(p => p.CheckStatusAsync<Permissions.StorageWrite>())
+                .ReturnsAsync(status);
+            Permissions
+                .Setup(p => p.RequestAsync<Permissions.StorageWrite>())
+                .ReturnsAsync(response);
+            return this;
+        }
+
+        public AudioUseCaseBuilder WithMicrophonePermission(PermissionStatus status, PermissionStatus response)
+        {
+            Permissions
+                .Setup(p => p.CheckStatusAsync<Permissions.Microphone>())
+                .ReturnsAsync(status);
+            Permissions
+                .Setup(p => p.RequestAsync<Permissions.Microphone>())
+                .ReturnsAsync(response);
+            return this;
+        }
+
+        public AudioUseCaseBuilder WithStartRecordingAudio(string recordingPath)
+        {
+            AudioRecorder
+                    .Setup(x => x.StartRecordingAudio(It.IsAny<string>()))
+                    .ReturnsAsync(recordingPath);
+            return this;
+        }
+
+        internal AudioUseCaseBuilder WithStopRecordingAudio(string recordingPath)
+        {
+            AudioRecorder
+                .Setup(x => x.StopRecordingAudio(It.IsAny<string>()));
+            return this;
+        }
+    }
+}
+
--- SpeakDanish/SpeakDanish.Tests/Domain/UseCases/AudioUseCasesTests.cs
+++ SpeakDanish/SpeakDanish.Tests/Domain/UseCases/AudioUseCasesTests.cs
@@ -13,16 +13,18 @@ using FluentAssertions;
 using System.Timers;
 using Microsoft.Extensions.DependencyInjection;
 using SpeakDanish.Contracts.Domain;
+using SpeakDanish.Tests.ViewModels;
+using SpeakDanish.Contracts;
 
 namespace SpeakDanish.Tests.Domain.UseCases
 {
-	public class AudioUseCasesTests
-	{
+    public class AudioUseCaseTests
+    {
         private Locale _danishLocale;
         private Action<ServiceCollection> _addAudioUseCase;
 
-        public AudioUseCasesTests()
-		{
+        public AudioUseCaseTests()
+        {
             Type localeType = typeof(Locale);
             ConstructorInfo constructor = localeType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new[] {
                 typeof(string),
@@ -33,74 +35,42 @@ namespace SpeakDanish.Tests.Domain.UseCases
 
             object[] constructorArgs = { "da", "", "Danish", "" };
             _danishLocale = (Locale)constructor.Invoke(constructorArgs);
-
-            _addAudioUseCase = (serviceCollection) => {
-                serviceCollection.AddTransient<IAudioUseCase, AudioUseCase>();
-            };
         }
 
         [Fact]
         public async void SpeakSentenceAsync_ShouldSpeakSentence_WhenCalled()
         {
             // Arrange
-            var mockTextToSpeech = new Mock<ITextToSpeech>();
-            
-
-            var setupDictionary = new Dictionary<Type, Action<Mock>>();
-            setupDictionary.Add(typeof(ITextToSpeech), mock =>
-            {
-                mockTextToSpeech = mock as Mock<ITextToSpeech>;
-                mockTextToSpeech
-                    .Setup(tts => tts.GetLocalesAsync())
-                    .ReturnsAsync(new List<Locale> { _danishLocale });
-                mockTextToSpeech
-                    .Setup(tts => tts.SpeakAsync(It.IsAny<string>(), It.IsAny<SpeechOptions>(), It.IsAny<CancellationToken>()))
-                    .Returns(Task.CompletedTask);
-            });
-
-            var audioUseCase = TestUtils
-                .CreateTestProvider(setupDictionary, _addAudioUseCase)
-                .GetService<IAudioUseCase>();
+            var sentence = "Sentence to speak";
+            var builder = new AudioUseCaseBuilder()
+                .WithSpeakAsync(new List<Locale>() { _danishLocale })
+                .Build();
 
             // Act
-            var result = await audioUseCase.SpeakSentenceAsync("Hej!", null);
+            var result = await builder.AudioUseCase.SpeakSentenceAsync(sentence, null);
 
             // Assert
-            result.Should().NotBeNull();
-            result.Success.Should().BeTrue();
-            mockTextToSpeech.Verify(tts => tts.SpeakAsync(It.IsAny<string>(), It.IsAny<SpeechOptions>(), It.IsAny<CancellationToken>()), Times.Once());
+            AssertThat(builder)
+                .IsSuccessful(result)
+                .VerifySpeakAsync(sentence, _danishLocale, Times.Once);
         }
 
         [Fact]
         public async void SpeakSentenceAsync_ShouldReturnError_WhenDanishLanguageNotFound()
         {
             // Arrange
-            var mockTextToSpeech = new Mock<ITextToSpeech>();
-
-            var setupDictionary = new Dictionary<Type, Action<Mock>>();
-            setupDictionary.Add(typeof(ITextToSpeech), mock =>
-            {
-                mockTextToSpeech = mock as Mock<ITextToSpeech>;
-                mockTextToSpeech
-                    .Setup(tts => tts.GetLocalesAsync())
-                    .ReturnsAsync(new List<Locale> { });
-                mockTextToSpeech
-                    .Setup(tts => tts.SpeakAsync(It.IsAny<string>(), It.IsAny<SpeechOptions>(), It.IsAny<CancellationToken>()))
-                    .Returns(Task.CompletedTask);
-            });
-
-            var audioUseCase = TestUtils
-                .CreateTestProvider(setupDictionary, _addAudioUseCase)
-                .GetService<IAudioUseCase>();
+            var sentence = "Sentence to speak";
+            var builder = new AudioUseCaseBuilder()
+                .WithSpeakAsync(new List<Locale>())
+                .Build();
 
             // Act
-            var result = await audioUseCase.SpeakSentenceAsync("Hej!", null);
+            var result = await builder.AudioUseCase.SpeakSentenceAsync(sentence, null);
 
             // Assert
-            result.Should().NotBeNull();
-            result.Success.Should().BeFalse();
-            result.Message.Should().NotBeNullOrWhiteSpace();
-            mockTextToSpeech.Verify(tts => tts.SpeakAsync(It.IsAny<string>(), It.IsAny<SpeechOptions>(), It.IsAny<CancellationToken>()), Times.Never());
+            AssertThat(builder)
+                .IsNotSuccessful(result)
+                .VerifySpeakAsync(sentence, null, Times.Never);   
         }
 
         [Fact]
@@ -108,41 +78,23 @@ namespace SpeakDanish.Tests.Domain.UseCases
         {
             // Arrange
             var recordingPath = "/path/to/recording.mp3";
-            var audioRecorderMock = new Mock<IAudioRecorder>();
-            var permissionsMock = new Mock<IPermissions>();
 
-            var setupDictionary = new Dictionary<Type, Action<Mock>>();
-            setupDictionary.Add(typeof(IPermissions), mock =>
-            {
-                permissionsMock = mock as Mock<IPermissions>;
-                permissionsMock.Setup(p => p.CheckStatusAsync<Permissions.Microphone>())
-                .ReturnsAsync(PermissionStatus.Granted);
-                permissionsMock.Setup(p => p.CheckStatusAsync<Permissions.StorageWrite>())
-                    .ReturnsAsync(PermissionStatus.Granted);
-            });
-            setupDictionary.Add(typeof(IAudioRecorder), mock =>
-            {
-                audioRecorderMock = mock as Mock<IAudioRecorder>;
-                audioRecorderMock
-                    .Setup(x => x.StartRecordingAudio(It.IsAny<string>()))
-                    .ReturnsAsync(recordingPath);
-            });
-
-            var audioUseCase = TestUtils
-                .CreateTestProvider(setupDictionary, _addAudioUseCase)
-                .GetService<IAudioUseCase>();
+            var builder = new AudioUseCaseBuilder()
+                .WithMicrophonePermission(PermissionStatus.Granted, PermissionStatus.Granted)
+                .WithStoragePermission(PermissionStatus.Granted, PermissionStatus.Granted)
+                .WithStartRecordingAudio(recordingPath)
+                .Build();
 
             // Act
-            var result = await audioUseCase.StartRecordingAsync(null);
+            var result = await builder.AudioUseCase.StartRecordingAsync(null);
 
             // Assert
-            result.Should().NotBeNull();
-            result.Success.Should().BeTrue();
-            result.Data.Should().Be(recordingPath);
-
-            permissionsMock.Verify(x => x.CheckStatusAsync<Permissions.Microphone>(), Times.Once);
-            permissionsMock.Verify(x => x.CheckStatusAsync<Permissions.StorageWrite>(), Times.Once);
-            audioRecorderMock.Verify(x => x.StartRecordingAudio(It.IsAny<string>()), Times.Once);
+            AssertThat(builder)
+                .IsSuccessful(result)
+                .ResultHasData(result, recordingPath)
+                .MicrophonePermission(PermissionStatus.Granted, PermissionStatus.Granted)
+                .StorageWritePermission(PermissionStatus.Granted, PermissionStatus.Granted)
+                .VerifyStartRecordingAudio("recording", Times.Once);
         }
 
         [Fact]
@@ -150,26 +102,94 @@ namespace SpeakDanish.Tests.Domain.UseCases
         {
             // Arrange
             var recordingPath = "/path/to/recording.mp3";
-            Mock<IAudioRecorder> audioRecorderMock = new Mock<IAudioRecorder>();
 
-            var setupDictionary = new Dictionary<Type, Action<Mock>>();
-            setupDictionary.Add(typeof(IAudioRecorder), mock =>
-            {
-                audioRecorderMock = mock as Mock<IAudioRecorder>;
-                audioRecorderMock.Setup(x => x.StopRecordingAudio(It.IsAny<string>()));
-            });
-
-            var audioUseCase = TestUtils
-                .CreateTestProvider(setupDictionary, _addAudioUseCase)
-                .GetService<IAudioUseCase>();
+            var builder = new AudioUseCaseBuilder()
+                .WithStopRecordingAudio(recordingPath)
+                .Build();
 
             // Act
-            var result = await audioUseCase.StopRecordingAsync(recordingPath);
+            var result = await builder.AudioUseCase.StopRecordingAsync(recordingPath);
 
             // Assert
-            result.Should().NotBeNull();
-            result.Success.Should().BeTrue();
-            audioRecorderMock.Verify(x => x.StopRecordingAudio(It.IsAny<string>()), Times.Once);
+            AssertThat(builder)
+                .IsSuccessful(result)
+                .VerifyStopRecordingAudio(recordingPath, Times.Once);
+        }
+
+        AudioUseCaseVerifier AssertThat(AudioUseCaseBuilder builder)
+        {
+            return new AudioUseCaseVerifier(builder);
+        }
+
+        internal class AudioUseCaseVerifier
+        {
+            private AudioUseCaseBuilder _builder;
+
+            public AudioUseCaseVerifier(AudioUseCaseBuilder builder)
+            {
+                _builder = builder;
+            }
+
+            internal AudioUseCaseVerifier IsSuccessful(Response result)
+            {
+                result.Should().NotBeNull();
+                result.Success.Should().BeTrue();
+                return this;
+            }
+
+            internal AudioUseCaseVerifier ResultHasData(Response<string> result, string path)
+            {
+                result.Data.Should().Be(path);
+                return this;
+            }
+
+            internal AudioUseCaseVerifier IsNotSuccessful(Response result)
+            {
+                result.Should().NotBeNull();
+                result.Success.Should().BeFalse();
+                result.Message.Should().NotBeNullOrWhiteSpace();
+                return this;
+            }
+
+            internal AudioUseCaseVerifier VerifySpeakAsync(string sentence, Locale locale, Func<Times> times)
+            {
+                _builder.TextToSpeech.Verify(
+                    tts => tts.SpeakAsync(
+                        sentence,
+                        It.Is<SpeechOptions>(x => x.Locale == locale),
+                        It.IsAny<CancellationToken>()
+                    ), times
+                );
+                return this;
+            }
+
+            internal AudioUseCaseVerifier MicrophonePermission(PermissionStatus checkStatus, PermissionStatus requestStatus)
+            {
+                Func<Times> requestTimes = checkStatus == PermissionStatus.Granted ? Times.Never : Times.Once;
+                _builder.Permissions.Verify(x => x.CheckStatusAsync<Permissions.Microphone>(), Times.Once);
+                _builder.Permissions.Verify(x => x.RequestAsync<Permissions.Microphone>(), requestTimes);
+                return this;
+            }
+
+            internal AudioUseCaseVerifier StorageWritePermission(PermissionStatus checkStatus, PermissionStatus requestStatus)
+            {
+                Func<Times> requestTimes = checkStatus == PermissionStatus.Granted ? Times.Never : Times.Once;
+                _builder.Permissions.Verify(x => x.CheckStatusAsync<Permissions.StorageWrite>(), Times.Once);
+                _builder.Permissions.Verify(x => x.RequestAsync<Permissions.StorageWrite>(), requestTimes);
+                return this;
+            }
+
+            internal AudioUseCaseVerifier VerifyStartRecordingAudio(string filename, Func<Times> once)
+            {
+                _builder.AudioRecorder.Verify(x => x.StartRecordingAudio(filename), Times.Once);
+                return this;
+            }
+
+            internal AudioUseCaseVerifier VerifyStopRecordingAudio(string filepath, Func<Times> once)
+            {
+                _builder.AudioRecorder.Verify(x => x.StopRecordingAudio(filepath), Times.Once);
+                return this;
+            }
         }
     }
 }
